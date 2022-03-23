29ae04017c40d6f1498d803835d3768ca6449a11
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-03-23 19:21:15 -0300

    Requisito 1: Create tests for App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente App', () => {
+  test('Verifica se os links de navegação estão presentes.', () => {
+    render(
+      <MemoryRouter><App /></MemoryRouter>,
+    );
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
+
+  it('Verifica se o link home direciona para a página correta.', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica se o link about home direciona para a página correta.', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  it('Verifica se o link Favorite Pokémon direciona para a página correta.', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Verifica se ao entrar em uma URL desconhecida é direcionado para a página correta',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/url-desconhecida');
+      const notFound = screen.getByText('Page requested not found');
+      expect(notFound).toBeInTheDocument();
+    });
+});
